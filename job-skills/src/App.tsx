2d721680771b2c4bcc142b6925415d95c01c6284
Jargil Santos <urban-fox@users.noreply.github.com>
2018-09-16 20:52:35 +1200

    UI-Material imported and component files created

--- job-skills/src/App.tsx
+++ job-skills/src/App.tsx
@@ -3,6 +3,50 @@ import './App.css';
 
 import logo from './logo.svg';
 
+/*
+// interfaces
+interface IJob {
+  uuid: string,
+  title: string,
+  normalised_title: string,
+  parent_uuid: any,
+}
+
+interface ISkill {
+  uuid: string,
+  name: string,
+  onet_element_id: any,
+  normalised_name: string,
+}
+
+
+// dummy data
+
+const jobs: IJob[] = [
+  {
+    uuid: "0001",
+    title: "Developer",
+    normalised_title: "Developer",
+    parent_uuid: null
+  },
+  {
+    uuid: "0002",
+    title: "Bus Driver",
+    normalised_title: "Bus Driver",
+    parent_uuid: null
+  },
+]
+
+const skills: ISkill[] = [
+  {
+    uuid: "0010",
+    name: "Programming",
+    onet_element_id: null,
+    normalised_name: "Programming",
+  }
+]
+*/
+
 class App extends React.Component {
   public render() {
     return (
