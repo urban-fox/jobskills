f85105963127615a334a27f8c058d31d6350a0f6
Jargil Santos <urban-fox@users.noreply.github.com>
2018-09-17 05:08:07 +1200

    Search text works

--- job-skills/src/components/item-card.tsx
+++ job-skills/src/components/item-card.tsx
@@ -1,13 +1,20 @@
 import { Card, CardContent } from "@material-ui/core";
 import Typography from '@material-ui/core/Typography';
 import * as React from "react";
+import { themes } from "./../theme";
 
-export class ItemCard extends React.Component {
+interface IState{
+  theme: any
+}
+export class ItemCard extends React.Component<{}, IState> {
   public props: any;
 
   constructor(props: any){
     super(props);
     this.props = props;
+    this.state = {
+      theme: themes.light
+    }
   }
 
   
@@ -15,20 +22,28 @@ export class ItemCard extends React.Component {
     alert(text);
     return text;
   }
+
+  public clickHandler(jobID: any){
+    this.props.clickHandler(jobID);
+    this.setState (
+      {theme: themes.dark}
+    )
+  }
   
   
   public render(){
     const title = this.props.title;
     const detail = this.props.detail;
-    const clickHandler = this.props.clickHandler;
+    // const clickHandler = this.props.clickHandler;
     // const clickHandler = this.test;
     return(
-      <Card onClick={clickHandler ? clickHandler.bind(this, this.props.jobID): null}>
+      <Card onClick={this.clickHandler ? this.clickHandler.bind(this, this.props.jobID): null} 
+        style={{backgroundColor: this.state.theme.background, color: this.state.theme.foreground}}>
         <CardContent>
-          <Typography variant="headline">
+          <Typography variant="headline" style={{color: this.state.theme.foreground}}>
             {title}
           </Typography>
-          <Typography variant='body1'>
+          <Typography variant='body1'style={{color: this.state.theme.foreground}}>
             {detail}
           </Typography>
         </CardContent>
--- job-skills/src/components/searchbar.tsx
+++ job-skills/src/components/searchbar.tsx
@@ -1,17 +1,26 @@
+import { Grid } from "@material-ui/core";
 import {AppBar, Button, Input, Toolbar, Typography} from '@material-ui/core/';
 import SearchIcon from '@material-ui/icons/Search';
 import * as React from 'react';
 
+interface IState{
+  searchString: string,
+  results: any
+}
 
-
-export class SearchBar extends React.Component<{}, {}> {
+export class SearchBar extends React.Component<{}, IState> {
   public props: any;
   constructor(props: any){
     super(props);
     this.props = props;
+    this.state = {
+      searchString: "",
+      results: ""
+    }
     this.receiveData = this.receiveData.bind(this)
     // this.changeState = this.changeState.bind(this)
     this.searchFunction = this.searchFunction.bind(this)
+    this.handleChange = this.handleChange.bind(this)
     /*
     this.state = {
       results: ''
@@ -33,45 +42,60 @@ export class SearchBar extends React.Component<{}, {}> {
   public searchFunction(e: any){
     // const dummyString = "developer";
     // const currentComponent = this;
-    fetch('http://api.dataatwork.org/v1/jobs/autocomplete?contains=developer', {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-      }
-    }).then((response : any) => {
-      if (!response.ok) {
-        this.setState({results: response.statusText});
-      }
-      else {
-        // response.json().then((data:any) => this.setState({results: data[0].class}))
-        // response.json().then((data:any) => alert("notOK! " + JSON.stringify(data)))
-        response.json().then((data: any) => this.props.setJobs(data)) // Lift response up to app level
-        // response.json().then((data: any) => currentComponent.changeState({results: data})) // set local state
-        // response.json().then((data: any) => currentComponent.receiveData(data)) // Lift response up to app level
-        // response.json().then((data: any) => currentComponent.setState({results: data})) // Lift response up to app level
-      }
-      return response
-    })
+    if (this.state.searchString){
+      fetch('http://api.dataatwork.org/v1/jobs/autocomplete?contains=' + this.state.searchString, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+        }
+      }).then((response : any) => {
+        if (!response.ok) {
+          this.setState({results: response.statusText});
+        }
+        else {
+          // response.json().then((data:any) => this.setState({results: data[0].class}))
+          // response.json().then((data:any) => alert("notOK! " + JSON.stringify(data)))
+          response.json().then((data: any) => this.props.setJobs(data)) // Lift response up to app level
+          // response.json().then((data: any) => currentComponent.changeState({results: data})) // set local state
+          // response.json().then((data: any) => currentComponent.receiveData(data)) // Lift response up to app level
+          // response.json().then((data: any) => currentComponent.setState({results: data})) // Lift response up to app level
+        }
+        return response
+      })
+    }
   }
 
-
+  public handleChange(event: any = "") {
+    this.setState({searchString: event.target.value? event.target.value: ""})
+    event.preventDefault();
+  }
 
   public render() {
     return (
       <AppBar position="static">
         <Toolbar>
-            <Typography variant="display2" color="inherit">
-              Job Skills
-            </Typography>
-
-            <SearchIcon />
-            <Input
-              placeholder="Search…"
-              disableUnderline={true}
-            />
-            <Button onClick={this.searchFunction}>
-              Search
-            </Button>
+          <Grid container={true} spacing={8}>
+            <Grid item={true} xs={4}>
+              <Typography variant="display2" color="inherit">
+                Job Skills
+              </Typography>
+            </Grid>
+            <Grid item={true} xs={6}>
+              <SearchIcon />
+              <Input
+                placeholder="Search…"
+                disableUnderline={true}
+                onChange={this.handleChange}
+              />
+            </Grid>
+            <Grid item={true} xs={2}>
+              <Button 
+                color="inherit"
+                onClick={this.searchFunction}>
+                Search
+              </Button>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     );
